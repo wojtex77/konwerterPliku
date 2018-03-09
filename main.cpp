1de762809eaae5b700deec6318223e6c61ca6f37
wojtex77 <wojciech.siwek.programista@gmail.com>
2018-03-09 21:43:26 +0100

    Dodanie funkcji wczytującej dane z pliku tekstowego do wektora

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <conio.h>
+#include <vector>
+
 
 using namespace std;
 
+void wczytajDaneDoStringa(vector <string> &dane){
+
+    fstream plik;
+    string pojedynczaLinia;
+    plik.open("wpisy.txt",ios::in);
+    if (plik.good() == true) {
+        while (!plik.eof()) {
+            getline(plik,pojedynczaLinia);
+            dane.push_back(pojedynczaLinia);
+        }
+        plik.close();
+        plik.clear();
+    }
+    cout << dane[0] << dane[3];
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector <string> *wczytaneDaneZPliku = new vector <string>(0);
+    wczytajDaneDoStringa(*wczytaneDaneZPliku);
     return 0;
 }
